d8977d821ef5f3130990a771d246a34d117114f2
doo2000 <ydslnge@gmail.com>
2019-04-30 04:56:03 -0700

    Beatiful day

--- src/edu/pcc/cis/HelloWorld/HelloWorld.java
+++ src/edu/pcc/cis/HelloWorld/HelloWorld.java
@@ -3,5 +3,6 @@ package edu.pcc.cis.HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo, World!");
+        System.out.println("It's a beautiful day");
     }
 }
