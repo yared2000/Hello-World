25d0c177c70f3563453de1094328672082d1233f
doo2000 <ydslnge@gmail.com>
2019-04-30 05:03:46 -0700

    print date

--- src/edu/pcc/cis/HelloWorld/HelloWorld.java
+++ src/edu/pcc/cis/HelloWorld/HelloWorld.java
@@ -1,8 +1,11 @@
 package edu.pcc.cis.HelloWorld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo, World!");
         System.out.println("It's a beautiful day");
+        System.out.println(new Date());
     }
 }
